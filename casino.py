1408b1a66f8cd1c7aff1d1469f26f8d79ad98a1c
Madinee <foumtraore22@gmail.com>
2019-09-08 18:31:16 +0200

    possibilite de rejouter tant que continuer=true

--- casino.py
+++ casino.py
@@ -6,51 +6,53 @@
 from random import randrange
 from math import ceil
 
+# variable globale
+continuer=True
+while continuer:# tant que l'utilisateur voudra rejouer le jeu
 
-print("saisissez un numéro compris entre 0 et 49")
-numero_saisi=input()
-numero_saisi=int(numero_saisi)
-while numero_saisi<0 or numero_saisi>49:
-	numero_saisi=input("veuillez saisir un numero compris entre 0 et 49\n")
-	try:
-		numero_saisi=int(numero_saisi)
-		break
-	except NameError:
-		print("veuillez saisir une valeur entière\n")
-		continue
-	#except ValueError:
-	   # print("veuillez saisir un entier \n")
-		#continue
-print("vous avez choisir:  ", numero_saisi)
-while 1:
-    try:
-        somme_mise=input("combien souhaitez vous miser?\n")
-        somme_mise=int(somme_mise)
-        assert somme_mise>0
-        break
-    except NameError:
-	    print("veuillez saisir une valeur entière\n")
-	    continue
-    except ValueError:
-	    print("veuillez saisir un entier \n")
-	    continue
-	# except AssertionError:
-	# 	print("veuillez saisir une valeur positive\n")
-	# 	continue
-print("vous avez miser ", somme_mise,"$\n")
-numero_gagnant=randrange(50)
-print("le numero generé est:\n ", numero_gagnant)
-if numero_gagnant==numero_saisi:
-	gain=somme_mise*3
-	gain=ceil(gain)
-	print("bravo vous avez gagner: ", gain)
-elif (numero_saisi%2==numero_gagnant%2):
-	gain=somme_mise/2
-	gain=ceil(gain)
-	print("mince vous ne gagner que la moité de votre  somme misée : ", gain)
-else:
-	print("vous avez perdu votre argent ")
-
-print("au revoir et a bientot!")
+	print("saisissez un numéro compris entre 0 et 49")
+	numero_saisi=input()
+	numero_saisi=int(numero_saisi)
+	while numero_saisi<0 or numero_saisi>49:
+		numero_saisi=input("veuillez saisir un numero compris entre 0 et 49\n")
+		try:
+			numero_saisi=int(numero_saisi)
+		except ValueError:
+			print("veuillez saisir un nombre\n")
+			continue
+		if numero_saisi<0:
+			print"le numero saisi est négatif\n"
+		if numero_saisi>49:
+			print"le numero saisi est superieur a 49\n"
+	print"**************vous avez choisir:  ", numero_saisi, "*******************\n"
+	somme_mise=0# je mets somme_mise a 0 pour pouvoir entrer dans la boucle
+	while somme_mise<=0:
+		somme_mise=input("combien souhaitez vous miser?\n")
+		try:
+			somme_mise=int(somme_mise)
+		except ValueError:
+		    print("veuillez saisir un entier \n")
+		    continue
+		if somme_mise<=0:
+			print"la somme miseée ne peut etre inferieur ou egal a zero\n"
+		
+	print"******************vous avez miser ", somme_mise,"$ ***********************\n"
+	numero_gagnant=randrange(50)
+	print"********************le numero generé est:", numero_gagnant,"******************\n"
+	if numero_gagnant==numero_saisi:
+		gain=somme_mise*3
+		gain=ceil(gain)
+		print"bravo vous avez gagner: ", gain
+	elif (numero_saisi%2==numero_gagnant%2):
+		gain=somme_mise/2
+		gain=ceil(gain)
+		print"mince vous avez perdu la moitié de votre argent : ", gain
+	else:
+		print"dommage !vous avez perdu votre argent \n"
+	quitter=raw_input("souhaiter vous quitter le jeu casison ou continuer a jouer(o/n)\n")
+	# print(quitter)
+	if quitter=="o" or quitter=="O":
+		continuer=False
+		print("==========================au revoir et a bientot!============================")
 
 
